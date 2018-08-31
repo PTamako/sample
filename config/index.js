88601a8a0f5ae46950a84f37f63958e6ce24756e
PTamako <ptamako@qq.com>
2018-08-31 15:31:31 +0800

    ok

--- config/index.js
+++ config/index.js
@@ -13,7 +13,6 @@ module.exports = {
       '/api/': 'http://localhost:8081/',
       changeOrigin: true,//是否跨域
     },
-
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
